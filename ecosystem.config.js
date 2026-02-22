8874573b718df41078e646cc33c80bb033884d11
Andrei Nossov <anossov@yahoo.com>
2026-02-22 07:42:06 -0800

    chore: remove PM2 log_date_format (Python handles timestamps now)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
             restart_delay: 5000,
             autorestart: true,
             max_memory_restart: "200M",
-            log_date_format: "YYYY-MM-DD HH:mm:ss",
+
             error_file: "logs/error.log",
             out_file: "logs/out.log",
             merge_logs: true,
